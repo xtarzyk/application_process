70420b00b5e7844f6144ce6752a2744da314a04e
KamilStarzyk <xtarzyk@tutanota.com>
2021-01-06 19:31:30 +0100

    task 'Searching for applicant by email' is done

--- data_manager.py
+++ data_manager.py
@@ -3,7 +3,7 @@ from typing import List, Dict
 from psycopg2 import sql
 from psycopg2.extras import RealDictCursor
 
-import database_common
+import database_common, re
 
 
 @database_common.connection_handler
@@ -21,6 +21,47 @@ def get_mentors_by_last_name(cursor: RealDictCursor, last_name: str) -> list:
     query = """
         SELECT first_name, last_name, city
         FROM mentor
+        WHERE LOWER(last_name) = LOWER(%(last_name)s)
         ORDER BY first_name"""
+    cursor.execute(query, {'last_name': last_name})
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_cities(cursor: RealDictCursor) -> list:
+    query = """
+        SELECT DISTINCT city from mentor;
+    """
     cursor.execute(query)
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_mentors_by_city(cursor: RealDictCursor, city: str) -> list:
+    query = """
+        SELECT first_name, last_name, city
+        FROM mentor
+        WHERE city = %(city)s"""
+    cursor.execute(query, {'city': city})
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_applicant_data_by_name(cursor: RealDictCursor, name: str) -> list:
+    query = """
+        SELECT CONCAT(first_name, ' ', last_name) AS full_name, phone_number
+        FROM applicant
+        WHERE first_name = %(name)s OR last_name = %(name)s
+        """
+    cursor.execute(query, {'name': name})
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_applicant_data_by_email_ending(cursor: RealDictCursor, email_ending: str) -> list:
+    cursor.execute("""
+        SELECT CONCAT(first_name, ' ', last_name) AS full_name, phone_number
+        FROM applicant
+        WHERE SUBSTR(email, POSITION('@' in email)) = %(email_ending)s
+        """, {'email_ending': email_ending})
     return cursor.fetchall()
--- main.py
+++ main.py
@@ -13,15 +13,31 @@ def index():
 @app.route('/mentors')
 def mentors_list():
     mentor_name = request.args.get('mentor-last-name')
+    city = request.args.get('city-input')
+    cities = data_manager.get_cities()
 
     if mentor_name:
         mentor_details = data_manager.get_mentors_by_last_name(mentor_name)
+    elif city:
+        mentor_details = data_manager.get_mentors_by_city(city)
     else:
         mentor_details = data_manager.get_mentors()
 
     # We get back a list of dictionaries from the data_manager (for details check 'data_manager.py')
 
-    return render_template('mentors.html', mentors=mentor_details)
+    return render_template('mentors.html', mentors=mentor_details, cities=cities)
+
+@app.route('/applicants-phone')
+def applicants_phone():
+    applicant_name = request.args.get('applicant-name')
+    applicant_details = data_manager.get_applicant_data_by_name(applicant_name)
+    return render_template('phone.html', applicants=applicant_details, name=applicant_name)
+
+@app.route('/applicants-email')
+def applicants_email():
+    applicant_email_ending = request.args.get('applicant-email-ending')
+    applicant_details = data_manager.get_applicant_data_by_email_ending(applicant_email_ending)
+    return render_template('email.html', endings=applicant_details, email_ending=applicant_email_ending)
 
 
 if __name__ == '__main__':
